41d0e7f3c1b0a920ecfa03f17ff5becf9d2e0996
Andy Situ <and.situ@gmail.com>
2015-08-07 16:50:19 -0700

    controller + added click event to table

--- bricks.js
+++ bricks.js
@@ -1,10 +1,30 @@
 var map = [];
 
+var controller = {
+	clicked(y, x) {
+		console.log(y, x);
+	}
+};
+
 function init() {
 	var cols = Math.floor((window.innerWidth / 9) );
 	var rows = Math.floor(window.innerHeight / 9 - 2);
 
 	makeTable(rows, cols);
+
+	function clicky(event) {
+		var target = event.target;
+
+		var str = /(\d*)_(\d*)/.exec(target.id);
+		if (str) {
+			var y = Number(str[1]); 
+			var x = Number(str[2]);
+
+			controller.clicked(y, x);
+		}
+	}
+
+	table.addEventListener("click", clicky, false)
 }
 
 window.onload = init;
